e116fec61cba32b02f53909536c31f64705e80cd
Harsh Jain <jain.hn@somaiya.edu>
2024-02-01 13:12:27 -0800

    Added validations and single value return to the ticker api

--- Assignments/Assignment2/flaskr/__init__.py
+++ Assignments/Assignment2/flaskr/__init__.py
@@ -1,11 +1,12 @@
 import os, requests
-from flask import Flask,request,jsonify
+from flask import Flask,request,jsonify,make_response
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def create_app(test_config=None):
-    # create and configure the app
+    # App configuration
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY=os.environ.get("SECRET_KEY"),
@@ -33,10 +34,19 @@ def create_app(test_config=None):
         search_string=request.args.get('ticker')
         search_string=(search_string,False)[search_string is None]
         if search_string:
-            stock_data=requests.get("{}/search?q={}&token={}".format(os.environ.get("FINHUB_ENDPOINT"),search_string,os.environ.get("FINHUB_API_KEY"))).json()
-        else:
-            stock_data={"Error":"No Search String Provided"}
+            stock_data=requests.get("{}/search?q={}&token={}".format(os.environ.get("FINHUB_ENDPOINT"),search_string,os.environ.get("FINHUB_API_KEY")))
+            if stock_data.status_code == 200:
+                stock_data=stock_data.json()["result"]
+                if stock_data:
+                    stock_data=stock_data[0]
+                else:
+                    stock_data=[]
+            else :
+                stock_data=(make_response(jsonify({"Error":"Finhub Api Error"}),502),stock_data.json())
             
+            
+        else:
+            stock_data= make_response(jsonify({"Error":"No Search String Provided"}),400)
         return stock_data
 
     return app
