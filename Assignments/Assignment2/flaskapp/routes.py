af3732538ba12bd2dfc40e9da3109a817ae8dad4
Harsh Jain <jain.hn@somaiya.edu>
2024-02-02 01:00:39 -0800

    Added validation middleware functions

--- Assignments/Assignment2/flaskapp/routes.py
+++ Assignments/Assignment2/flaskapp/routes.py
@@ -8,106 +8,78 @@ from dateutil.relativedelta import relativedelta
 routes=Blueprint('main_routes',__name__)
 
 
+#TODO:write middleware function for errors
 
+def error_middleware(stock_data,valid_attribute=None):
+    if stock_data.status_code == 200:
+        stock_data=stock_data.json()
+        validation_exp=stock_data if valid_attribute ==  None else stock_data[valid_attribute]
+        if not validation_exp:
+            stock_data=make_response(jsonify({"Error":"Stock not found"}),404)
+    else :
+        stock_data=make_response(jsonify({"Error":"External API Error"}),502)
+        
+    return stock_data
 
-#write middleware function for errors
-
-@routes.route('/company-data', methods = ["GET"])
-def stock_ticker():
-    search_string=request.args.get('ticker')
-    search_string=(search_string,False)[search_string is None]
-    if search_string:
-        stock_data=requests.get("{}/stock/profile2?symbol={}&token={}".format(os.environ.get("FINHUB_ENDPOINT"),search_string,os.environ.get("FINHUB_API_KEY")))
-        if stock_data.status_code == 200:
-            stock_data=stock_data.json()
-            if not stock_data:
-                stock_data=make_response(jsonify({"Error":"Ticker not found"}),404)
-        else :
-            stock_data=make_response(jsonify({"Error":"Finhub API Error"}),502)
+def validation_middleware(ticker,URL,valid_attribute=None):
+    stock_ticker=(ticker,False)[ticker is None]
+    if stock_ticker:
+        return error_middleware(requests.get(URL),valid_attribute)
     else:
         stock_data= make_response(jsonify({"Error":"No Search String Provided"}),400)
     return stock_data
 
+
+
+@routes.route('/company-data', methods = ["GET"])
+def stock_ticker():
+    search_string=request.args.get('ticker')
+    URL="{}/stock/profile2?symbol={}&token={}".format(os.environ.get("FINHUB_ENDPOINT"),search_string,os.environ.get("FINHUB_API_KEY"))
+    stock_data=validation_middleware(search_string,URL)
+    return stock_data
+    
+
 @routes.route('/stock-summary', methods = ["GET"])
 def stock_summary():
     ticker=request.args.get('ticker')
-    ticker=(ticker,False)[ticker is None]
-    if ticker:
-        stock_data=requests.get("{}/quote?symbol={}&token={}".format(os.environ.get("FINHUB_ENDPOINT"),ticker,os.environ.get("FINHUB_API_KEY")))
-        
-        if stock_data.status_code == 200:
-            stock_data=stock_data.json()
-            if not stock_data["d"]:
-                stock_data=make_response(jsonify({"Error":"Stock not found"}),404)
-        else :
-            stock_data=make_response(jsonify({"Error":"Finhub API Error"}),502)
-    else:
-        stock_data= make_response(jsonify({"Error":"No Search String Provided"}),400)
+    URL="{}/quote?symbol={}&token={}".format(os.environ.get("FINHUB_ENDPOINT"),ticker,os.environ.get("FINHUB_API_KEY"))
+    validation_attribute="d"
+    stock_data=validation_middleware(ticker,URL,validation_attribute)
     return stock_data
 
 
 @routes.route('/recommendation-trends', methods = ["GET"])
 def recommendation_trends():
     ticker=request.args.get('ticker')
-    ticker=(ticker,False)[ticker is None]
-    if ticker:
-        stock_data=requests.get("{}/stock/recommendation?symbol={}&token={}".format(os.environ.get("FINHUB_ENDPOINT"),ticker,os.environ.get("FINHUB_API_KEY")))
-        
-        if stock_data.status_code == 200:
-            stock_data=stock_data.json()
-            if not stock_data:
-                stock_data=make_response(jsonify({"Error":"Stock not found"}),404)
-            else:
-                stock_data=stock_data[0]
-        else :
-            stock_data=make_response(jsonify({"Error":"Finhub API Error"}),502)
-    else:
-        stock_data= make_response(jsonify({"Error":"No Search String Provided"}),400)
+    URL="{}/stock/recommendation?symbol={}&token={}".format(os.environ.get("FINHUB_ENDPOINT"),ticker,os.environ.get("FINHUB_API_KEY"))
+    stock_data=validation_middleware(ticker,URL)
+    if isinstance(stock_data,list) and len(stock_data)>=1:
+        stock_data=stock_data[0]
     return stock_data
 
 
-
-
+#TODO:Filter check for news without images
 @routes.route('/company-news', methods = ["GET"])
 def company_news():
     ticker=request.args.get('ticker')
     ticker=(ticker,False)[ticker is None]
     TODAY=date.today()
     PREVIOUS_MONTH=TODAY+relativedelta(months=-1)
-    if ticker:
-        stock_data=requests.get("{}/company-news?symbol={}&from={}&to={}&token={}".format(os.environ.get("FINHUB_ENDPOINT"),ticker,PREVIOUS_MONTH,TODAY,os.environ.get("FINHUB_API_KEY")))
-        
-        if stock_data.status_code == 200:
-            stock_data=stock_data.json()
-            if not stock_data:
-                stock_data=make_response(jsonify({"Error":"Stock not found"}),404)
-            else:
-                stock_data=stock_data[:4]
-        else :
-            stock_data=make_response(jsonify({"Error":"Finhub API Error"}),502)
-    else:
-        stock_data= make_response(jsonify({"Error":"No Search String Provided"}),400)
+    URL="{}/company-news?symbol={}&from={}&to={}&token={}".format(os.environ.get("FINHUB_ENDPOINT"),ticker,PREVIOUS_MONTH,TODAY,os.environ.get("FINHUB_API_KEY"))
+    stock_data=validation_middleware(ticker,URL)
+    if isinstance(stock_data,list) and len(stock_data)>=1:
+        stock_data=stock_data[:4]
     return stock_data
 
 
 @routes.route('/chart-data', methods = ["GET"])
 def chart_data():
     ticker=request.args.get('ticker')
-    ticker=(ticker,False)[ticker is None]
     TODAY=date.today()
     PREVIOUS_MONTH=TODAY+relativedelta(months=-6,days=-1)
-    if ticker:
-        stock_data=requests.get("{}/{}/range/1/day/{}/{}?adjusted=true&sort=asc&apiKey={}".format(os.environ.get("POLYGON_ENDPOINT"),ticker,PREVIOUS_MONTH,TODAY,os.environ.get("POLYGON_API_KEY")))
-
-        if stock_data.status_code == 200:
-            stock_data=stock_data.json()
-            if not stock_data:
-                stock_data=make_response(jsonify({"Error":"Stock not found"}),404)
-            else:
-                stock_data=stock_data["results"]
-        else :
-            stock_data=make_response(jsonify({"Error":"Polygon API Error"}),502)
-    else:
-        stock_data= make_response(jsonify({"Error":"No Search String Provided"}),400)
+    URL="{}/{}/range/1/day/{}/{}?adjusted=true&sort=asc&apiKey={}".format(os.environ.get("POLYGON_ENDPOINT"),ticker,PREVIOUS_MONTH,TODAY,os.environ.get("POLYGON_API_KEY"))
+    stock_data=validation_middleware(ticker,URL)
+    if isinstance(stock_data["results"],list) and len(stock_data)>=1:
+        stock_data=stock_data["results"]
     return stock_data
 
