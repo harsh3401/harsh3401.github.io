1e6983ddd1b080225c218c205377a35ca1875476
Harsh Jain <jain.hn@somaiya.edu>
2024-03-02 23:24:02 -0800

    Added all homepage endpoints

--- Assignments/Assignment3/index.ts
+++ Assignments/Assignment3/index.ts
@@ -2,11 +2,20 @@ import axios, { AxiosResponse } from "axios";
 import dotenv from "dotenv";
 import express, { Express, Request, Response } from "express";
 dotenv.config();
+
 const PORT = process.env.PORT ?? 8080;
 const app: Express = express();
 
 // TODO:Any Fix
+function formatDate(date: Date) {
+  const formattedDate = new Intl.DateTimeFormat("fr-CA", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  }).format(date);
 
+  return formattedDate;
+}
 function error_middleware(
   stock_data: any,
   res: Response,
@@ -99,12 +108,19 @@ app.get("/recommendation-trends", (req: Request, res: Response) => {
 
 app.get("/company-news", (req: Request, res: Response) => {
   const query = String(req.query["ticker"]);
+  let TODAY: Date | string = new Date();
+  //TODO:Value check
+  let PREVIOUS_MONTH: Date | string = new Date(TODAY);
+  PREVIOUS_MONTH.setDate(TODAY.getDate() - 30);
+  PREVIOUS_MONTH = formatDate(PREVIOUS_MONTH);
+  TODAY = formatDate(TODAY);
+
   console.log(
-    `${process.env.FINHUB_ENDPOINT}/stock/profile2?symbol=${query}&token=${process.env.FINHUB_API_KEY}`
+    `${process.env.FINHUB_ENDPOINT}/company-news?symbol=${query}&token=${process.env.FINHUB_API_KEY}&from=${PREVIOUS_MONTH}&to=${TODAY}`
   );
   axios
     .get(
-      `${process.env.FINHUB_ENDPOINT}/stock/profile2?symbol=${query}&token=${process.env.FINHUB_API_KEY}`
+      `${process.env.FINHUB_ENDPOINT}/company-news?symbol=${query}&token=${process.env.FINHUB_API_KEY}&from=${PREVIOUS_MONTH}&to=${TODAY}`
     )
     .then((response: AxiosResponse) => {
       error_middleware(response.data, res, response.status.valueOf());
@@ -115,14 +131,46 @@ app.get("/company-news", (req: Request, res: Response) => {
     });
 });
 
-app.get("/company-details", (req: Request, res: Response) => {
+app.get("/insider-sentiment", (req: Request, res: Response) => {
+  const query = String(req.query["ticker"]);
+  //From value hardcoded
+  axios
+    .get(
+      `${process.env.FINHUB_ENDPOINT}/stock/insider-sentiment?symbol=${query}&token=${process.env.FINHUB_API_KEY}&from=2022-01-01`
+    )
+    .then((response: AxiosResponse) => {
+      error_middleware(response.data, res, response.status.valueOf(), "data");
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("error");
+    });
+});
+
+app.get("/company-peers", (req: Request, res: Response) => {
+  const query = String(req.query["ticker"]);
+
+  axios
+    .get(
+      `${process.env.FINHUB_ENDPOINT}/stock/peers?symbol=${query}&token=${process.env.FINHUB_API_KEY}`
+    )
+    .then((response: AxiosResponse) => {
+      error_middleware(response.data, res, response.status.valueOf());
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("error");
+    });
+});
+
+app.get("/company-earnings", (req: Request, res: Response) => {
   const query = String(req.query["ticker"]);
   console.log(
-    `${process.env.FINHUB_ENDPOINT}/stock/profile2?symbol=${query}&token=${process.env.FINHUB_API_KEY}`
+    `${process.env.FINHUB_ENDPOINT}/stock/earnings?symbol=${query}&token=${process.env.FINHUB_API_KEY}`
   );
   axios
     .get(
-      `${process.env.FINHUB_ENDPOINT}/stock/profile2?symbol=${query}&token=${process.env.FINHUB_API_KEY}`
+      `${process.env.FINHUB_ENDPOINT}/stock/earnings?symbol=${query}&token=${process.env.FINHUB_API_KEY}`
     )
     .then((response: AxiosResponse) => {
       error_middleware(response.data, res, response.status.valueOf());
