3e9f84314c9bfdfaa564d8f1ff8d45f99bed547e
Harsh Jain <jain.hn@somaiya.edu>
2024-04-05 17:54:05 -0700

    IOS project initialized.

---
+++ Assignments/Assignment 4/WebTechIosApp/WebTechIosApp/WebTechIosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebTechIosAppApp.swift
+//  WebTechIosApp
+//
+//  Created by Harsh Jain on 05/04/24.
+//
+
+import SwiftUI
+
+@main
+struct WebTechIosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
